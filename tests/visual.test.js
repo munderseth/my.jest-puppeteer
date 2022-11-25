ff7ccaad0c3a4a2092d0369175835ed0f0dfc944
munderseth <marku@s2technologies.com>
2022-11-25 09:52:29 -0800

    example using clip

--- tests/visual.test.js
+++ tests/visual.test.js
@@ -2,16 +2,41 @@
 const { toMatchImageSnapshot } = require('jest-image-snapshot');
 expect.extend({ toMatchImageSnapshot });
 
+const delay= 0
+//await new Promise(resolve => setTimeout(resolve, 4000));
+const sleep = (secs) => {
+    return new Promise((resolve) => {
+        setTimeout(resolve, secs*1000)
+    });
+}
+
 describe('Generic Issues', () => {
+
+    beforeAll(async () => {
+        await page.setViewport({ width: 1440, height: 1080 });
+    });
 
     test('Add Issue Reference', async () => {
         await page.goto("https://s2.testspace.com/projects/testspace-com:demo");
-        const screenshot = await page.screenshot();
+        if (process.env.HEADLESS !== 'false') {
+            await sleep(delay);
+        }
+        const screenshot = await page.screenshot({
+            clip: {
+                x: 0,
+                y: 0,
+                width: 1325,
+                height: 260
+            }
+        });
         expect(screenshot).toMatchImageSnapshot();
     });
 
     test('Add Multiple Issue References', async () => {
         await page.goto("https://s2.testspace.com/projects/testspace-com:demo/spaces/main/specs");
+        if (process.env.HEADLESS !== 'false') {
+            await sleep(delay);
+        }
         const screenshot = await page.screenshot();
         expect(screenshot).toMatchImageSnapshot();
     });
