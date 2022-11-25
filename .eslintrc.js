c75a52bf852380f7d351d66d388c2eb3660dd596
munderseth <marku@s2technologies.com>
2022-11-25 07:08:26 -0800

    workflow added

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "node": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": 11
+    },
+    "rules": {
+    }
+}
